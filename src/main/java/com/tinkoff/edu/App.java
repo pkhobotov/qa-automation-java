ba29c8e2732a2ecf08b7cda44a5f83d77445c14f
p.khobotov <p.khobotov@tinkoff.ru>
2021-08-11 10:00:54 +0300

    format code and up junit version

--- src/main/java/com/tinkoff/edu/App.java
+++ src/main/java/com/tinkoff/edu/App.java
@@ -2,6 +2,6 @@ package com.tinkoff.edu;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println ( "Hello, world!" );
+        System.out.println("Hello, world!");
     }
 }
--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+
     @Test
     public void shouldAnswerWithTrue() {
 
-        assertTrue ( true );
+        assertTrue(true);
     }
 }
