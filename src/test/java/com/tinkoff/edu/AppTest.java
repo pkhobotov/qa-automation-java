4c0431b145e073c664c89cd9a9c08f143b2df149
p.khobotov <p.khobotov@tinkoff.ru>
2021-09-07 10:38:07 +0300

    split tests

--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -6,7 +6,7 @@ import com.tinkoff.edu.app.common.LoanType;
 import com.tinkoff.edu.app.common.ResponseType;
 import com.tinkoff.edu.app.controller.DefaultLoanCalcController;
 import com.tinkoff.edu.app.controller.LoanCalcController;
-import com.tinkoff.edu.app.exceptions.*;
+import com.tinkoff.edu.app.exceptions.RequestException;
 import com.tinkoff.edu.app.repository.ArrayLoanCalcRepository;
 import com.tinkoff.edu.app.repository.LoanCalcRepository;
 import com.tinkoff.edu.app.service.IpNotFriendlyLoanCalcService;
@@ -128,45 +128,12 @@ public class AppTest {
     }
 
     @Test
-    public void shouldReturnExceptionWhenLoanTypeIsNull() {
-        request = new LoanRequest(4,
-                                  8000,
-                                  null,
-                                  "Крткм".repeat(5));
-        assertThrows(LoanTypeException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
     public void shouldReturnApplicationUUID() throws RequestException {
         request = buildDefaultRequest();
         LoanApplication application = sut.createRequest(request);
         UUID requestId = application.getRequestId();
         assertEquals(application.getResponse(),
                      sut.getApplicationStatus(requestId));
-    }
-
-    @Test
-    public void shouldReturnExceptionWhenNoRequestStored() {
-        assertThrows(GetApplicationException.class,
-                     () -> sut.getApplicationStatus(UUID.randomUUID()));
-    }
-
-    @Test
-    public void shouldReturnExceptionWhenNoRequestFound() throws RequestException {
-        request = buildDefaultRequest();
-        sut.createRequest(request);
-        assertThrows(GetApplicationException.class,
-                     () -> sut.getApplicationStatus(UUID.randomUUID()));
-    }
-
-    @Test
-    public void shouldReturnExceptionWhenNoRequestFoundToSetStatus() throws RequestException {
-        request = buildDefaultRequest();
-        sut.createRequest(request);
-        assertThrows(GetApplicationException.class,
-                     () -> sut.setApplicationStatus(UUID.randomUUID(),
-                                                    ResponseType.DENIED));
     }
 
     @Test
@@ -178,65 +145,5 @@ public class AppTest {
         assertEquals(ResponseType.DENIED,
                      sut.setApplicationStatus(requestId,
                                               ResponseType.DENIED));
-    }
-
-    @Test
-    public void getErrorOnShortFIO() {
-        request = new LoanRequest(4,
-                                  8000,
-                                  LoanType.PERSON,
-                                  "Крткм");
-        assertThrows(FIOLengthException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
-    public void getErrorOnLongFIO() {
-        request = new LoanRequest(4,
-                                  8000,
-                                  LoanType.PERSON,
-                                  "Крткм".repeat(21));
-        assertThrows(FIOLengthException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
-    public void getErrorOnNullFIO() {
-        request = new LoanRequest(4,
-                                  8000,
-                                  LoanType.PERSON,
-                                  null);
-        assertThrows(FIOLengthException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
-    public void getErrorOnInvalidCharContainingFIO() {
-        request = new LoanRequest(4,
-                                  8000,
-                                  LoanType.PERSON,
-                                  "Крткм".repeat(3) + "/");
-        assertThrows(IllegalCharacterException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
-    public void getErrorOnSmallerAmount() {
-        request = new LoanRequest(4,
-                                  0.001,
-                                  LoanType.PERSON,
-                                  defaultFio);
-        assertThrows(IllegalRequestAmountException.class,
-                     () -> sut.createRequest(request));
-    }
-
-    @Test
-    public void getErrorOnBiggerAmount() {
-        request = new LoanRequest(4,
-                                  1000000000,
-                                  LoanType.PERSON,
-                                  defaultFio);
-        assertThrows(IllegalRequestAmountException.class,
-                     () -> sut.createRequest(request));
     }
 }
